82dc6dc7d78090fd7430878512f510a17d2a8c85
Harry <harryaberg@gmail.com>
2019-10-21 14:27:19 +0100

    deploying latest

--- rollup.config.js
+++ rollup.config.js
@@ -29,103 +29,103 @@ export default [
       commonjs()
     ]
   },
-  // {
-  //   input,
-  //   output: {
-  //     file: minifyExtension(packageJSON.main),
-  //     format: "cjs",
-  //     sourcemap: true
-  //   },
-  //   plugins: [
-  //     babel({
-  //       exclude: "node_modules/**"
-  //     }),
-  //     external(),
-  //     resolve(),
-  //     commonjs(),
-  //     uglify(),
-  //   ]
-  // },
-  // // UMD
-  // {
-  //   input,
-  //   output: {
-  //     file: packageJSON.browser,
-  //     format: "umd",
-  //     sourcemap: true,
-  //     name: "reactSampleComponentsLibrary",
-  //     globals: {
-  //       react: "React",
-  //       "@emotion/styled": "styled",
-  //       "@emotion/core": "core"
-  //     }
-  //   },
-  //   plugins: [
-  //     babel({
-  //       exclude: "node_modules/**"
-  //     }),
-  //     external(),
-  //     resolve(),
-  //     commonjs()
-  //   ]
-  // },
-  // {
-  //   input,
-  //   output: {
-  //     file: minifyExtension(packageJSON.browser),
-  //     format: "umd",
-  //     sourcemap: true,
-  //     name: "reactSampleComponentsLibrary",
-  //     globals: {
-  //       react: "React",
-  //       "@emotion/styled": "styled",
-  //       "@emotion/core": "core"
-  //     }
-  //   },
-  //   plugins: [
-  //     babel({
-  //       exclude: "node_modules/**"
-  //     }),
-  //     external(),
-  //     resolve(),
-  //     commonjs(),
-  //     terser()
-  //   ]
-  // },
-  // // ES
-  // {
-  //   input,
-  //   output: {
-  //     file: packageJSON.module,
-  //     format: "es",
-  //     sourcemap: true,
-  //     exports: "named"
-  //   },
-  //   plugins: [
-  //     babel({
-  //       exclude: "node_modules/**"
-  //     }),
-  //     external(),
-  //     resolve(),
-  //     commonjs()
-  //   ]
-  // },
-  // {
-  //   input,
-  //   output: {
-  //     file: minifyExtension(packageJSON.module),
-  //     format: "es",
-  //     sourcemap: true,
-  //     exports: "named"
-  //   },
-  //   plugins: [
-  //     babel({
-  //       exclude: "node_modules/**"
-  //     }),
-  //     external(),
-  //     resolve(),
-  //     commonjs(),
-  //     terser()
-  //   ]
-  // }
+  {
+    input,
+    output: {
+      file: minifyExtension(packageJSON.main),
+      format: "cjs",
+      sourcemap: true
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs(),
+      uglify(),
+    ]
+  },
+  // UMD
+  {
+    input,
+    output: {
+      file: packageJSON.browser,
+      format: "umd",
+      sourcemap: true,
+      name: "reactSampleComponentsLibrary",
+      globals: {
+        react: "React",
+        "@emotion/styled": "styled",
+        "@emotion/core": "core"
+      }
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs()
+    ]
+  },
+  {
+    input,
+    output: {
+      file: minifyExtension(packageJSON.browser),
+      format: "umd",
+      sourcemap: true,
+      name: "reactSampleComponentsLibrary",
+      globals: {
+        react: "React",
+        "@emotion/styled": "styled",
+        "@emotion/core": "core"
+      }
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs(),
+      terser()
+    ]
+  },
+  // ES
+  {
+    input,
+    output: {
+      file: packageJSON.module,
+      format: "es",
+      sourcemap: true,
+      exports: "named"
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs()
+    ]
+  },
+  {
+    input,
+    output: {
+      file: minifyExtension(packageJSON.module),
+      format: "es",
+      sourcemap: true,
+      exports: "named"
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs(),
+      terser()
+    ]
+  }
 ];
