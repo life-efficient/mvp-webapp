22e21830fa39030efa5f90dad5d4d27fe5e1f501
Harry <harryaberg@gmail.com>
2019-10-23 08:07:31 +0100

    deploying latest

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { default as Button } from "components/Button";
 export { default as Loading } from "components/Loading"
 // export { default as Login } from "components/Login"
-// export { default as Navbar } from "components/Navbar"
+export { default as Navbar } from "components/Navbar"
