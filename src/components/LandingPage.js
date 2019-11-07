fea0c3a138db681b03390b2d8db70e68e0d92ac2
Harry <harryaberg@gmail.com>
2019-11-07 15:52:28 +0000

    deploying latest

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,7 +6,7 @@ import { Background } from ".."
 
 export default (props) => { return (
     <>
-        <Navbar btn='Login'/>
+        <Navbar btn='Login' to={props.navLink}/>
         <Fold hero={props.hero} heading={props.heading ? props.heading : 'Add a `heading` prop!'} subtitle={props.subtitle ? props.subtitle : 'Add a `subtitle` prop!'} text={props.btnText ? props.btnText : 'Pass a "btnText" prop to the LandingPage component!'} action={props.action ? props.action : () => {alert('Pass an `action` prop to the fold component!')}} />
         {
             props.sections ?
