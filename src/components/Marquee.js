56158fa820f6af392b24a054335f69f6e7d37e5a
Harry <harryaberg@gmail.com>
2019-11-28 08:55:25 +0000

    deploying latest

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -4,6 +4,7 @@ import { jsx, css } from "@emotion/core"
 const style = css`
     display: flex;
     overflow: hidden;
+    max-width: 100%;
     img {
         max-height: 200px;
         animation: translate 50s linear infinite;
