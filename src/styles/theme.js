94fe06b592a76c33d0089f52e0ceee8a3d3ee585
Harry <harryaberg@gmail.com>
2019-11-29 12:31:15 +0000

    deploying latest

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -46,8 +46,7 @@ export const panel = css`
   width: 100%;
   background: linear-gradient(var(--secondary), var(--secondary_grad));
   margin: 20px auto;
-  // width: 80%;
-  overflow: hidden;
+  width: 80%;
   max-width: 400px;
   border-radius: 3px;
   padding: 20px;
