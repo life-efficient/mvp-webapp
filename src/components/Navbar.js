055ac420d49cef19bb7ad6e3a4e37f221cb5618b
Harry <harryaberg@gmail.com>
2019-12-15 21:31:41 +0000

    deploying latest

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -92,7 +92,7 @@ var Navbar = (props) => {
     var back_to = props.back
     // back_to = back_to === true ? '/' : back_to
     
-    var content = back_to ? 'back' : props.name
+    var content = back_to ? 'back' : props.name // case 1: back prop is not given -> set content of button to name of app. back_to remains as null
     // if (props.roots && props.roots.includes(window.pathname)) { // if array of roots contains this path
     if (back_to && window.pathname === back_to) { // if array of roots contains this path
         content = props.name // then it should show a home and redirect to home
