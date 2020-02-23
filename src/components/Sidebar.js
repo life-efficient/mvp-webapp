8021863662b745c7d8b27167f5af5f3c4f5abe6c
Harry <harryaberg@gmail.com>
2020-02-23 23:53:45 +0000

    added sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react"
+import { css } from "@emotion/core"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from "react-router-dom";
+
+const style = css`
+    // position: absolute;
+    // background-color: var(--color2);
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    border-radius: 30px;
+    // width: auto;
+    .item {
+        font-family: var(--font1);
+        font-weight: 900;
+        color: var(--color2);
+        display: flex;
+        width: 160px;
+        flex-direction: row;
+        align-items: center;
+        justify-content: left;
+        padding: 10px;
+        margin: 5px;
+        cursor: pointer;
+        transition-duration: 1s;
+        border-radius: 50px;
+        :hover {
+            background-color: var(--color2);
+            color: var(--color1) !important;
+            .faicon {
+                color: var(--color1) !important;
+            }
+        }
+        .faicon, img {
+            padding-right: 10px;
+            transition-duration: 1s;
+            font-size: 35px;
+            color: var(--color2);
+
+            height: 35px;
+            width: 35px;
+        }
+    }
+    img {
+    }
+`
+
+export default class extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div css={style}>
+                {this.props.items.map(i=>{return ( 
+                    <Link to={i.to} className="item"> 
+                        {i.faIcon ? <FontAwesomeIcon icon={i.faIcon} className="faicon"/> : <img src={i.icon} />}
+                        {i.title}
+                    </Link>
+                )})}
+            </div>
+        )
+    }
+}
---
+++ src/components/Sidebar.md
@@ -0,0 +1,33 @@
+
+A navbar, but on the side.
+
+### Props
+#### **`items`**
+The `items` prop should be a list of objects with the following keys.
+
+- `faIcon`: this prop should be a component from fontAwesome's react library. FontAwesome has a huge collection of icons which are all treated like font. This means that you can change their color dynamically, rather than being stuck with using an image that can't change.
+- alternatively to `faIcon`, you can pass in the src for an image that you'd like to use as an icon. It should be imported by doing something along the lines of `import my_src from ./my_img_path/img.png`
+- `title`: the title will be shown next to the icon when the page is large enough
+
+``` jsx
+import { Sidebar } from "mvp-webapp";
+import { connect } from "react-redux";
+import notification from "../images/icons/notification.png";
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+<>
+<Sidebar items={[
+    {
+        'title': 'Notifications',
+        icon: notification,
+        // faIcon: <FontAwesomeIcon icon={faCoffee} color={'#ff822e'} size='50px'/>
+        faIcon: faCoffee,
+        to: '/to_item'
+    },
+    {
+        'title': 'Notifications',
+        icon: notification
+    },
+]}/>
+</>
+```
