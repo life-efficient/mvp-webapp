5707140dc46f873c0c7de96bb51c51ead06adb90
Harry <harryaberg@gmail.com>
2020-03-01 21:14:39 +0000

    l

--- src/components/Login.js
+++ src/components/Login.js
@@ -270,6 +270,7 @@ class Login extends Component {
                   {title: 'New password', type:'password', id:'new_password'},
                 ],
                 onSubmit: (e) => {
+                  console.log(e)
                   console.log('submitting:', e.email, e.code. e.new_password)
                   Auth.forgotPasswordSubmit(e.email, e.code, e.new_password)}
               }
