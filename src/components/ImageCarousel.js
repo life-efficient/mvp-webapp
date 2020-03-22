ddadd8dcb4f8ac0d2685dc0b49ced23aa1402cbd
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-03-22 21:58:27 +0000

    Added ImageCarousel export to index.js

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -19,11 +19,43 @@ class ImageCarousel extends Component{
         }
     }
 
+    //functions for changing slide 
+
+    prevSlide() {
+        const lastIndex = this.props.images.length - 1
+        const {currentImageIndex} = this.state
+        const shouldResetIndex = currentImageIndex === 0;
+        const index = shouldResetIndex ? lastIndex : currentImageIndex - 1
+
+        this.setState({
+            currentImageIndex : index
+        })
+    }
+
+    nextSlide() {
+        const lastIndex = this.props.images.length - 1
+        const {currentImageIndex} = this.state
+        const shouldResetIndex = currentImageIndex === lastIndex
+        const index = shouldResetIndex ? 0 : currentImageIndex + 1
+
+        this.setState({
+            currentImageIndex : index
+        })
+    }
+
+
+
     render(){
         return(
             <div>
                 <div css={styleArrow('left')} className="arrow" onClick={console.log("Clicked Left")}></div>
-                <ImageSlide src={this.props.images[this.state.currentImageIndex]} />
+                
+                <ImageSlide 
+                src={
+                    this.props.images[this.state.currentImageIndex] //gets url of next image based on current state and img prop
+                } 
+                />
+
                 <div css={styleArrow('right')} className="arrow" onClick={console.log("Clicked Right")}></div>
 
             </div>
