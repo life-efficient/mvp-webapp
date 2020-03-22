34e98172213c31b00c801b5dd33a9f08697f2616
haron1100 <hshams@hotmail.co.uk>
2020-03-22 00:45:23 +0000

    fixed chart.js error

--- src/utils.js
+++ src/utils.js
@@ -13,41 +13,6 @@ export const makeid = (length=8) => {
     };
     return text;
 }
-  
-export const getEpoch = (from, to, unit) => {
-    console.log(unit)
-    switch (unit) {
-        case 'days':
-            var now = Math.round(new Date().getTime() / 1000)  
-            var from = now - 60 * 60 * 24 * from
-            var to = now - 60 * 60 * 24 * to
-            return [from.toString(), to.toString()]
-        case 'weeks':
-            var now = Math.round(new Date().getTime() / 1000)  
-            var from = now - 60 * 60 * 24 * 7 * from
-            var to = now - 60 * 60 * 24 * 7 * to
-            return [from.toString(), to.toString()]
-        case 'months':
-            var now = Math.round(new Date().getTime() / 1000)  
-            var from = now - 60 * 60 * 24 * 30 * from
-            var to = now - 60 * 60 * 24 * 30 * to
-            return [from.toString(), to.toString()]
-        default:
-            return 'fail'
-    }
-}
-  
-export const hexToRgb = (hex, opacity) => {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    var c = {
-    r: parseInt(result[1], 16),
-    g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16),
-    a: opacity ? opacity : 1
-    }
-    return `rgba(${c.r}, ${c.g}, ${c.b}, ${c.a})`
-}
-
 
 export function importAll(directory) {
     var r = require.context(directory, false)
