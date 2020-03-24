0a266a28fb575b60fffef9197f7d0bb67f5f081e
KilianSL <seifk007.319@wcgs.foliotrust.uk>
2020-03-24 01:02:03 +0000

    Improved styling for image carousel, added dots

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -4,24 +4,121 @@ import {css} from '@emotion/core'
 
 //carousel class
 
+// css styling
+const style = css`
+    box-sizing: border-box;
+
+    > .carousel-container {
+        max-width: 1000px;
+        height: 300px;
+        position: relative;
+        margin: auto;
+        
+        > .prev, .next {
+            cursor: pointer;
+            position: absolute;
+            top: 50%;
+            width: auto;
+            margin-top: -22px;
+            padding: 16px;
+            color: white;
+            font-weight: bold;
+            font-size: 18px;
+            transition: 0.6s ease;
+            border-radius: 0 3px 3px 0;
+            user-select: none;
+        }
+        > .next{
+            right: 0;
+            border-radius: 3px 0 0 3px;
+        }
+        > .prev:hover, .next:hover {
+            background-color: rgba(0,0,0,0.8);
+        }
+
+        > .fade {
+            -webkit-animation-name: fade;
+            -webkit-animation-duration: 1.5s;
+            animation-name: fade;
+            animation-duration: 1.5s;
+          }
+    }
+
+    .dot-container{
+        text-align: center;
+    }
+    .dot {
+        cursor: pointer;
+        height: 15px;
+        width: 15px;
+        margin: 0 2px;
+        background-color: #bbb;
+        border-radius: 50%;
+        display: inline-block;
+        transition: background-color 0.6s ease;
+    }
+      
+    .active, .dot:hover {
+        background-color: #717171;
+    }
+`
+
+
 class ImageCarousel extends Component{
 
     constructor(props){
         super(props)
+        this.state = {
+            imageIndex : 0
+        }
+    }
+
+    // Logic for changing images
+
+    nextImage = () => {
+        const lastIndex = this.props.images.length - 1;
+        const shouldCycle = this.state.imageIndex === lastIndex;
+        const newIndex = shouldCycle ? 0 : this.state.imageIndex + 1;
+
+        this.setState({
+            imageIndex : newIndex
+        })
+    }
+
+    prevImage = () => {
+        const lastIndex = this.props.images.length - 1;
+        const shouldCycle = this.state.imageIndex === 0;
+        const newIndex = shouldCycle ? lastIndex : this.state.imageIndex - 1;
+
+        this.setState({
+            imageIndex : newIndex
+        })
     }
 
     render(){
         return(
-            <div>
+            <div css={style}>
                 <div className="carousel-container">
-                    {this.props.images.map(
-                    (img) => {
-                        return(
-                            <ImageSlide src={img} caption="Default Caption" />
-                        )
-                    }
-                )}
+                    <ImageSlide src={this.props.images[this.state.imageIndex]} caption="Default Caption"/>
+
+                {/* Next and previous buttons */}
+                <a className="prev" onClick={this.prevImage}>&#10094;</a>
+                <a className="next" onClick={this.nextImage}>&#10095;</a>
+
                 </div>
+                <br />
+                
+                {/* Circles indicating slide */}
+                <div className="dot-container">
+                    {this.props.images.map(
+                        (img) => {
+                            return(
+                                <span className="dot"></span>
+                            )
+                        }
+                    )}
+                </div>
+                
             </div>
             
         )
@@ -32,10 +129,29 @@ export default ImageCarousel
 
 // Imageslide component
 
+const style_imageSlide = css`
+    height: 100%;
+    > img{
+        width: 100%;
+        height: 100%;
+    }
+    > .image-caption {
+        color: #f2f2f2;
+        font-size: 15px;
+        padding: 8px 12px;
+        position: absolute;
+        bottom: 8px;
+        width: 100%;
+        text-align: center;
+    }
+        
+`
+
+
 export const ImageSlide = ({src, caption}) => {
     return(
-        <div className="imageslide fade">
-            <img src={src} style={{width:'100%'}} />
+        <div className="imageslide fade" css={style_imageSlide}>
+            <img src={src} />
             <div className="image-caption">{caption}</div>
         </div>
     )
