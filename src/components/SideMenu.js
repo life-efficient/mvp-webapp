3461a8cc7d43e95b6eef1eff1e07677577631350
Harry <harryaberg@gmail.com>
2020-03-26 02:31:13 +0000

    added console log to show sidemenu content

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -107,6 +107,7 @@ class SideMenu extends Component {
 
     render() {
         console.log('logged in:', this.props.logged_in)
+        console.log('content:', this.props.content)
         return (
             <div id="mySidenav" css={style} style={this.getStyle()} ref={this.setWrapperRef}>
                 <div className="closebtn" onClick={this.props.toggleMenu}>&times;</div>
@@ -125,6 +126,7 @@ class SideMenu extends Component {
         )
     }
 }
+// export default SideMenu
 
 const mapStateToProps = (state) => {
     return  {
